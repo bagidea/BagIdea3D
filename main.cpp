1ce5ac084c23c0134034e00288e023cb8c0d88bd
bagidea <aodbboy@hotmail.com>
2015-07-21 03:58:20 +0700

    Update Main

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 {
 	bis = new BI3D("BagIdea3D", 800, 600);
 
-	bis->SetEvent((void*)&Update, BI3D_EVENT_START);
+	bis->SetEvent((void*)&Start, BI3D_EVENT_START);
 	bis->SetEvent((void*)&Update, BI3D_EVENT_UPDATE);
 
 	bis->Start();
